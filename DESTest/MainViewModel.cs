0f4b56f23b66e7e5b18aab1de1e4248a77b1fc18
飞 程 <cf20090901@outlook.com>
2019-10-18 21:18:01 +0800

    添加项目文件。

---
+++ DESTest/MainViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DESTest
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string filePath;   //comment
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set { SetProperty(ref filePath, value); }
+        }
+
+        private int byteLength;   //comment
+
+        public int ByteLength
+        {
+            get { return byteLength; }
+            set { SetProperty(ref byteLength, value); }
+        }
+
+        public MainViewModel()
+        {
+            ByteLength = 8;
+        }
+
+    }
+
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName]string propertyName = "", Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+
+            onChanged?.Invoke();
+
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+
+            if (changed == null)
+                return;
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ DESTest/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using Shell32;
+
+namespace DESTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel mainViewModel = new MainViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = mainViewModel;
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(mainViewModel.ByteLength.ToString());
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = true;
+            dialog.Title = "请选择文件";
+            dialog.Filter = "任何文件(*.*)|*.*";
+
+            var result = dialog.ShowDialog();
+            if (result == true)
+            {
+                //AddMusic(dialog.FileNames);
+                mainViewModel.FilePath = dialog.FileName;
+            }
+
+            //FileStream fileStream4 = new FileStream("", FileMode.Open, FileAccess.Read, FileShare.Read, 10, FileOptions.None);
+        }
+    }
+}
